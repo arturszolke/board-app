382049c262bb5eb8f049ad6a9244137a2e283bf3
Artur Szolke <aszolke002@gmail.com>
2023-09-06 12:24:48 +0200

    Draggable

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,30 @@ export default {
     data() {
         return {
             status: [
-                { id: 1, name: 'Todo', tasks: [] },
+                {
+                    id: 1, name: 'Todo', tasks: [
+                        {
+                            id: 1,
+                            title: 'Task 1',
+                            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget tempor aliquam, nunc libero ultricies nunc, quis aliquet nisl nunc eu nisl. Sed vitae nisl eget nisl aliquet aliquam. Sed vitae nisl eget nisl aliquet aliquam.',
+                        },
+                        {
+                            id: 2,
+                            title: 'Task 2',
+                            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget tempor aliquam, nunc libero ultricies nunc, quis aliquet nisl nunc eu nisl. Sed vitae nisl eget nisl aliquet aliquam. Sed vitae nisl eget nisl aliquet aliquam.',
+                        }
+                    ]
+                },
                 { id: 2, name: 'In Progress', tasks: [] },
-                { id: 3, name: 'Done', tasks: [] },
+                {
+                    id: 3, name: 'Done', tasks: [
+                        {
+                            id: 1,
+                            title: 'Task 1',
+                            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget tempor aliquam, nunc libero ultricies nunc, quis aliquet nisl nunc eu nisl. Sed vitae nisl eget nisl aliquet aliquam. Sed vitae nisl eget nisl aliquet aliquam.',
+                        },
+                    ]
+                },
                 { id: 4, name: 'Test', tasks: [] },
             ]
         }
--- src/components/BoardStatus.vue
+++ src/components/BoardStatus.vue
@@ -1,7 +1,12 @@
 <script lang="ts">
+import Draggable from 'vuedraggable'
+
 export default {
     name: 'BoardStatus',
-    props: ['status']
+    props: ['status'],
+    components: {
+        Draggable
+    },
 }
 </script>
 
@@ -10,6 +15,26 @@ export default {
         <div class="boardStatus rounded">
             <div class="fw-bolder statusName rounded-top">
                 <h2 class="py-3">{{ status.name }}</h2>
+            </div>
+            <!-- <div v-for="(task, id) in status.tasks" :key="id">
+                {{ task.title }}
+                {{ task.description }}
+            </div> -->
+            <div>
+                <Draggable class="draggable" ghost-class="ghost" drag-class="dragClass" group="tasks"
+                    fallback-class="fallbackStyleClass" :force-fallback="true" :animation="200" :list="status.tasks"
+                    item-key="id">
+                    <template #item="{ element: task }">
+                        <div class="drag bg-primary rounded-2 m-2 p-2">
+                            <h4>
+                                {{ task.title }}
+                            </h4>
+                            <p>
+                                {{ task.description }}
+                            </p>
+                        </div>
+                    </template>
+                </Draggable>
             </div>
         </div>
     </div>
@@ -32,5 +57,25 @@ export default {
 .statusName {
     background-color: #3b3b3b;
     color: #EEE5E9;
+}
+
+.draggable {
+    min-height: 400px;
+}
+
+.drag {
+    cursor: grab;
+}
+
+.fallbackStyleClass {
+    opacity: 0.75 !important;
+}
+
+.ghost {
+    visibility: hidden;
+}
+
+.ghost>div {
+    visibility: hidden !important;
 }
 </style>
