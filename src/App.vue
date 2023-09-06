38bf86996dd5073460ed362347e180d4697a82d9
Artur Szolke <aszolke002@gmail.com>
2023-09-06 10:26:43 +0200

    BoardStatus from structure

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
-<script setup lang="ts">
+<script lang="ts">
 import BoardStatus from './components/BoardStatus.vue'
 
 export default {
   name: 'App',
   components: {
     BoardStatus
-  }
+  },
+  data() {
+    return {
+      status: [
+        { id: 1, name: 'Todo', tasks: [] },
+        { id: 2, name: 'In Progress', tasks: [] },
+        { id: 3, name: 'Done', tasks: [] }
+      ]
+    }
+  },
 }
+
 </script>
 
 <template>
-  <div id="app">
-    <BoardStatus />
+  <div v-for="(state, id) in status" :key="id" class="app">
+    <BoardStatus :status="state" />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- src/components/BoardStatus.vue
+++ src/components/BoardStatus.vue
@@ -1,7 +1,32 @@
-<script setup lang="ts">
-
+<script lang="ts">
+export default {
+    name: 'BoardStatus',
+    props: ['status']
+}
 </script>
 
-<template></template>
+<template>
+    <div class="wrapper">
+        <div class="boardStatus">
+            <div>
+                {{ status.name }}
+            </div>
+        </div>
+    </div>
+</template>
 
-<style></style>
+<style scoped>
+.wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.boardStatus {
+    border: 1px solid white;
+    width: 200px;
+    height: 200px;
+    margin: 10px;
+    float: right;
+}
+</style>
