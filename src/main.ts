34e0d341d48d759565d8cc3e1dfc98c51771225d
Artur Szolke <aszolke002@gmail.com>
2023-09-07 09:14:52 +0200

    bootstrap-icons

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap-icons/font/bootstrap-icons';
 import "bootstrap"
+
 
 createApp(App).mount('#app')
